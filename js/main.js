121250409d4969f80c4efaed20e4cdb6d36aa711
DimaSilver <dimka7600@ukr.net>
2020-12-21 07:12:12 +0300

    Застилизовал сэкшены search и footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function () {
-
-})
+    $('.search-top__link').on('click', function(){
+        $(this).toggleClass('search-top__link--active')
+    });
+    $('.footer__social').on("click", ".footer__social-item", function() {
+        $(this).addClass('footer__social-item--active').siblings().removeClass('footer__social-item--active')
+    });
+    $('.footer__by').on('click', function(){
+        $(this).toggleClass('footer__by--active')
+    });
+});
