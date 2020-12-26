e5093a0a4e39502ac44840088e159aefa9b13593
DimaSilver <dimka7600@ukr.net>
2020-12-26 08:54:48 +0300

    Застилизовал сэкшен hotels-search

--- js/main.js
+++ js/main.js
@@ -65,10 +65,20 @@ $(function () {
         $(this).addClass('search-form__button--active').siblings().removeClass('search-form__button--active')
     });
     $('.menu__list').on("click", ".menu__list-item", function () {
-        $(this).addClass('menu__list-item--active').siblings().removeClass('menu__list-item--active')
+        $(this).addClass('menu__list-item--click').siblings().removeClass('menu__list-item--click')
     });
-
     $(".hotels-search__top-pricediapazon").ionRangeSlider();
-
+    $('.hotels-search__bottom-filter__sort-list').on("click", ".hotels-search__bottom-filter__sort-list__item", function () {
+        $(this).addClass('hotels-search__bottom-filter__sort-list__item--click').siblings().removeClass('hotels-search__bottom-filter__sort-list__item--click')
+    });
+    $('.hotels-search__bottom-filter__arrows').on("click", ".hotels-search__bottom-filter__arrow", function () {
+        $(this).addClass('hotels-search__bottom-filter__arrow--active').siblings().removeClass('hotels-search__bottom-filter__arrow--active')
+    });
+    $('.hotels-search__item-content__links').on("click", ".hotels-search__item-content__link", function () {
+        $(this).addClass('hotels-search__item-content__link--active').siblings().removeClass('hotels-search__item-content__link--active')
+    });
+    $('.hotels-search__item-content__title').on("click", function () {
+       $(this).toggleClass('hotels-search__item-content__title--active')
+    });
 
 });
